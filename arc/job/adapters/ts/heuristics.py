6bbdc0fa7622e012ae5965bbc4f1bac69ba0d2e9
Alon Grinberg Dana <alon@technion.ac.il>
2022-01-17 00:16:26 +0200

    heuristics

--- arc/job/adapters/ts/heuristics.py
+++ arc/job/adapters/ts/heuristics.py
@@ -205,6 +205,7 @@ class HeuristicsAdapter(JobAdapter):
         self.set_files()  # Set the actual files (and write them if relevant).
 
         self.restrarted = bool(job_num)  # If job_num was given, this is a restarted job, don't save as initiated jobs.
+        self.additional_job_info = None
 
         check_argument_consistency(self)
 
