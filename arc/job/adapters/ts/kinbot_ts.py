b3c617dcd91d8ca0a1172e587ce7b54636eac69a
Alon Grinberg Dana <alon@technion.ac.il>
2022-01-17 00:16:31 +0200

    kinbot

--- arc/job/adapters/ts/kinbot_ts.py
+++ arc/job/adapters/ts/kinbot_ts.py
@@ -229,6 +229,7 @@ class KinBotAdapter(JobAdapter):
         self.set_files()  # Set the actual files (and write them if relevant).
 
         self.restrarted = bool(job_num)  # If job_num was given, this is a restarted job, don't save as initiated jobs.
+        self.additional_job_info = None
 
         check_argument_consistency(self)
 
