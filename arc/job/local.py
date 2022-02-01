b46c6fc4f389008ef842948ac199c3b8da32de45
Alon Grinberg Dana <alon@technion.ac.il>
2022-02-02 00:58:48 +0200

    f! local HTCondor

--- arc/job/local.py
+++ arc/job/local.py
@@ -239,7 +239,8 @@ def submit_job(path):
     elif cluster_soft == 'htcondor' and 'submitting' in stdout[0].lower():
         # Submitting job(s).
         # 1 job(s) submitted to cluster 443069.
-        job_id = stdout[1].split()[-1].split('.')[0]
+        if len(stdout) and len(stdout[1].split()) and len(stdout[1].split()[-1].split('.')):
+            job_id = stdout[1].split()[-1].split('.')[0]
     else:
         raise ValueError(f'Unrecognized cluster software: {cluster_soft}')
     job_status = 'running' if job_id else job_status
